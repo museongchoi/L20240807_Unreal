356e25e9fb067e58c597348c2a5450025d382e27
museongchoi <antjd3303@hs.ac.kr>
2024-08-07 14:57:29 +0900

    add C++ Class

---
+++ Source/L20240807_Git/L20240807_Git.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "L20240807_Git.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, L20240807_Git, "L20240807_Git" );
